1fe935c96de25ebfe560939b7c240fa11ff495bf
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-04-03 16:29:49 +0900

    초기 커밋.

---
+++ ConsoleApplication30/ConsoleApplication30.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+void main()
+
+{
+
+	int x = 0, y = 0;
+	printf("점의좌표x,y)?");
+	scanf_s("%d %d", &x, &y);
+
+	if (x > 0 && y > 0) {
+		printf("1사분면에있습니다");
+	}
+	else if (x < 0 && y > 0) {
+		printf("2사분면에있습니다");
+	}
+	else if (x < 0 && y < 0) {
+		printf("3사분면에있습니다");
+	}
+	else if (x > 0 && y < 0) {
+		printf("4사분면에있습니다");
+	}
+	else if (x = 0) {
+		printf("x축에있습니다");
+	}
+	else {
+		printf("y축에있습니다");
+	}
+
+}
